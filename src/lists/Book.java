5e9a3ddd416d5f6b4f14b39f11275d74d3c53ead
Tetik <tetikmustafa03@gmail.com>
2024-01-28 23:39:30 +0300

    Added properties to Book and Movie

--- src/lists/Book.java
+++ src/lists/Book.java
@@ -1,4 +1,40 @@
 package lists;
 
 public class Book { // this class will keep the properties of book object.
+    private String bookName;
+    private String bookAuthor;
+    private String bookPublisher;
+    private int pageNumber;
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getBookAuthor() {
+        return bookAuthor;
+    }
+
+    public void setBookAuthor(String bookAuthor) {
+        this.bookAuthor = bookAuthor;
+    }
+
+    public String getBookPublisher() {
+        return bookPublisher;
+    }
+
+    public void setBookPublisher(String bookPublisher) {
+        this.bookPublisher = bookPublisher;
+    }
+
+    public int getPageNumber() {
+        return pageNumber;
+    }
+
+    public void setPageNumber(int pageNumber) {
+        this.pageNumber = pageNumber;
+    }
 }
--- src/lists/Movie.java
+++ src/lists/Movie.java
@@ -1,4 +1,58 @@
 package lists;
 
 public class Movie { // this class will keep the properties of movie object.
+    private String movieName;
+    private String movieDirector;
+    private String moviePlatform;
+    private String movieLeadRole;
+    private double imdbRating;
+    private int movieDuration;
+
+    public String getMovieName() {
+        return movieName;
+    }
+
+    public void setMovieName(String movieName) {
+        this.movieName = movieName;
+    }
+
+    public String getMovieDirector() {
+        return movieDirector;
+    }
+
+    public void setMovieDirector(String movieDirector) {
+        this.movieDirector = movieDirector;
+    }
+
+    public String getMoviePlatform() {
+        return moviePlatform;
+    }
+
+    public void setMoviePlatform(String moviePlatform) {
+        this.moviePlatform = moviePlatform;
+    }
+
+    public String getMovieLeadRole() {
+        return movieLeadRole;
+    }
+
+    public void setMovieLeadRole(String movieLeadRole) {
+        this.movieLeadRole = movieLeadRole;
+    }
+
+    public double getImdbRating() {
+        return imdbRating;
+    }
+
+    public void setImdbRating(double imdbRating) {
+        this.imdbRating = imdbRating;
+    }
+
+    public int getMovieDuration() {
+        return movieDuration;
+    }
+
+    public void setMovieDuration(int movieDuration) {
+        this.movieDuration = movieDuration;
+    }
 }
